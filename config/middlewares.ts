6b7815a99508fafcacb40f2d0bf07de9d3322b49
kenneth27dunne <kenneth_27_dunne@msn.com>
2025-01-10 11:53:50 +0000

    Update middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,22 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'default-src': ["'self'"],
+          'script-src': ["'self'", "'unsafe-inline'"], // Allow inline scripts (not recommended for production)
+          'style-src': ["'self'", "'unsafe-inline'"],
+          'img-src': ["'self'", 'data:', 'blob:'], // Allow image sources
+          'connect-src': ["'self'", 'https:'], // Allow API connections
+          'media-src': ["'self'", 'data:', 'blob:'], // Allow media sources
+          'frame-src': ["'self'"],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
